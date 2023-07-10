4ee34aca4bb0874dcb8eeecd36bc46b71cbf5e7a
florence wangechi <florencewangechim@gmail.com>
2023-07-10 14:25:05 +0300

    flask-sql

--- server/app.py
+++ server/app.py
@@ -19,15 +19,70 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if animal:
+        response_body = f''
+
+        response_body += f'<ul>ID :{Animal.id}</ul>'
+        response_body += f'<ul>Name :{Animal.name}</ul>'
+        response_body += f'<ul>Species : {Animal.species}</ul>'
+        response_body += f'<ul>Species: {animal.species}</ul>'
+        response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+        response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+
+    
+        response = make_response(response_body, 200)
+    else:
+        response_body = '<h1>404 Animal not found</h1>'
+        response = make_response(response_body, 404)
+    
+    return response
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if zookeeper:
+        response_body = f''
+
+        response_body += f'ID: {zookeeper.id}</ul>'
+        response_body += f'<ul>Name: {zookeeper.name}</ul>'
+        response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+        response_body += f'<ul>Animals:</ul>'
+            
+        response_body += '<ul>'
+
+        for animal in zookeeper.animals:
+            response_body += f'<ul>{animal.name} - {animal.species}</ul>'
+        response_body += '</ul>'
+        response = make_response(response_body, 200)
+    else:
+        response_body = '<h1>404 Zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+    
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if enclosure:
+        response_body = f''
+
+        response_body += f'<ul>ID: {enclosure.id}</ul>'
+        response_body += f'<ul>Environment: {enclosure.environment}</ul>'
+        response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+        response_body += f'<ul>Animals:</ul>'
+            
+        response_body += '<ul>'
+        for animal in enclosure.animals:
+            response_body += f'<li>{animal.name} - {animal.species}</li>'
+        response_body += '</ul>'
+        response = make_response(response_body, 200)
+    else:
+        response_body = '<h1>404 Enclosure not found</h1>'
+        response = make_response(response_body, 404)
+    
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,35 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+
+    # relationship between Zookeeper and Animal
+    animals = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+
+    # relationship between Enclosure and Animal
+    animals = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+
+    #  foreign keys for the relationships
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+
+
+
+
